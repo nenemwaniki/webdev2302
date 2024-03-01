25246e8bc221c84fc0583bcd5cdb8863099496b0
nenemwaniki <65731988+nenemwaniki@users.noreply.github.com>
2024-03-01 14:17:25 +0000

    first real commit=trial

--- timetable/schedule/views.py
+++ timetable/schedule/views.py
@@ -4,5 +4,5 @@ import datetime
 def index(request):
     day=datetime.datetime.now().weekday()
     time=datetime.datetime.now().time()
-    return render(request,'index.html',{'day':day,'time':time})
+    return render(request,'schedule/index.html',{'day':day,'time':time})
     
