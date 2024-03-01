339deef0d50f5303db07ee6e4f49b14bd227d2e7
nenemwaniki <65731988+nenemwaniki@users.noreply.github.com>
2024-03-01 14:21:04 +0000

    secend trial

--- timetable/schedule/views.py
+++ timetable/schedule/views.py
@@ -4,5 +4,8 @@ import datetime
 def index(request):
     day=datetime.datetime.now().weekday()
     time=datetime.datetime.now().time()
-    return render(request,'schedule/index.html',{'day':day,'time':time})
+    return render(request,'schedule/index.html',{
+        'day':day,
+        'time':time}
+        )
     
