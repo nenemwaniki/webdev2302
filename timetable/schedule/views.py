64fac79951b03cada70c76bb0a0594610aec4887
nenemwaniki <65731988+nenemwaniki@users.noreply.github.com>
2024-03-01 14:07:09 +0000

    fixed time

--- timetable/schedule/views.py
+++ timetable/schedule/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 def index(request):
-    return render(request, "schedule/index.html")
+    day=datetime.datetime.now().weekday()
+    time=datetime.datetime.now().time()
+    return render(request,'index.html',{'day':day,'time':time})
+    
