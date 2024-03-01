be4c89ab69a3970bb8598f4fb0e64fab5ba4e223
nenemwaniki <65731988+nenemwaniki@users.noreply.github.com>
2024-03-01 14:30:57 +0000

    from gpt

--- timetable/schedule/views.py
+++ timetable/schedule/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 import datetime
 # Create your views here.
 def index(request):
-    day=datetime.datetime.now().weekday()
-    time=datetime.datetime.now().time()
-    return render(request,'schedule/index.html',{
-        'day':day,
-        'time':time}
-        )
+    day = datetime.datetime.now().weekday()
+    time = datetime.datetime.now().time()
+    return render(request, 'schedule/index.html', {
+        'day': day,
+        'time': time
+    })
     
