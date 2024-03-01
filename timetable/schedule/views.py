0291ccb0ceb0f214ed8d92dac8265ca2cf759258
nenemwaniki <65731988+nenemwaniki@users.noreply.github.com>
2024-03-01 15:11:24 +0000

    change entries to home folder

--- timetable/schedule/views.py
+++ timetable/schedule/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-import datetime
-# Create your views here.
+import pandas as pd
+
+def get_student_timetable(student_id):
+    filename = f"entries/student{student_id}.csv"
+
+    try:
+        df = pd.read_csv(filename)
+        return df[['Day', 'Time', 'Subject']]
+    except FileNotFoundError:
+        return None
+
 def index(request):
-    day = datetime.datetime.now().weekday()
-    time = datetime.datetime.now().time()
+    # Assuming student IDs are 1, 2, and 3
+    student_ids = [1, 2, 3]
+    student_timetables = {}
+
+    for student_id in student_ids:
+        student_timetables[student_id] = get_student_timetable(student_id)
+        
+
     return render(request, 'schedule/index.html', {
-        'day': day,
-        'time': time
+        'student_timetables': student_timetables,
     })
-    
