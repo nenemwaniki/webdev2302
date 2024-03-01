0cc2b0f3b10040ffe1ca31e7474c234f793bcfcd
nenemwaniki <65731988+nenemwaniki@users.noreply.github.com>
2024-03-01 12:27:02 +0000

    new page

--- timetable/schedule/apps.py
+++ timetable/schedule/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SscheduleConfig(AppConfig):
+class scheduleConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Sschedule"
+    name = "schedule"
