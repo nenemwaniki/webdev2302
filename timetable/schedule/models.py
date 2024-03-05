80a1c33b6a1237a2074ba3da2e7c79ce40f8ff7f
Munene Mwaniki <nenemwaniki@gmail.com>
2024-03-05 10:35:10 +0300

    student model added

--- timetable/schedule/models.py
+++ timetable/schedule/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    registration_number = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    program = models.CharField(max_length=100)
+    year_of_study = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
